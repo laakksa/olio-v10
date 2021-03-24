208bbf25afde1fd68cdd09907d9e474d914dde06
sakulaakkonen <saku.laakkonen@student.lut.fi>
2021-03-24 19:12:40 +0200

    T5. Added buttons and functionality for previous and next pages.

--- app/src/main/java/com/olio/webbrowser/MainActivity.java
+++ app/src/main/java/com/olio/webbrowser/MainActivity.java
@@ -13,40 +13,80 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.ListIterator;
 import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
     WebView web;
     EditText addressBar;
+    int cursor;
+    ArrayList<String> cache;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        cache = new ArrayList<>();
+        cursor = 0;
         web = (WebView) findViewById(R.id.webView);
         web.setWebViewClient(new WebViewClient());
         web.getSettings().setJavaScriptEnabled(true);
-        web.loadUrl("file:///android_asset/index.html");
         addressBar = (EditText) findViewById(R.id.addressBar);
         addressBar.setMaxLines(1);
+        //Loads page from url when enter key is pressed.
         addressBar.setOnKeyListener(new View.OnKeyListener() {
             @Override
             public boolean onKey(View v, int keyCode, KeyEvent event) {
                 if((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)){
                     String urlString = addressBar.getText().toString();
-                    loadPage(urlString);
+                    loadPage(urlString, true);
                     return true;
                 }
                 return false;
             }
         });
     }
-    public void loadPage(String urlinput){
-        String urlString = "https://" + urlinput;
+
+    //Loads page from url, sets cursor to last of list if user loads a new page.
+    //Stores last 10 pages and removes the oldest one if a new one is added.
+    public void loadPage(String urlinput, boolean newinput){
+        String urlString;
+        if(!urlinput.contains("https://")) {
+            urlString = "https://" + urlinput;
+        } else{
+            urlString = urlinput;
+        }
         web.loadUrl(urlString);
         addressBar.setText(urlString);
+        if (newinput){
+            if(cache.size() == 10){
+                cache.remove(0);
+            }
+            cache.add(urlString);
+            cursor = cache.size() - 1;
+        }
     }
+
+
     public void refreshPage(View v){
         web.reload();
+    }
+
+    //Loads the previous page if there is one
+    public void previousPage(View v){
+        if(cursor >= 1){
+            loadPage(cache.get(cursor-1), false);
+            cursor--;
+        }
+    }
+
+    //Loads the next page if there is one
+    public void nextPage(View v){
+        if (cursor < cache.size() - 1){
+            loadPage(cache.get(cursor+1), false);
+            cursor++;
+        }
     }
 
     public void shoutOut(View v){
